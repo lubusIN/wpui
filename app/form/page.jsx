973450fc4437b729d08c0f951030fbfdd33d5a6e
Nikhil Sharma <nikhil777707@gmail.com>
2023-09-22 17:03:16 +0530

    updated one component

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -3,15 +3,19 @@
 import {
     Icon,
     Button,
+    Card,
+    CardBody,
+    DropZone,
     TextControl,
     TextareaControl,
-    __experimentalVStack as VStack,
-    __experimentalHeading as Heading,
+    FormFileUpload,
     __experimentalText as Text,
+    __experimentalVStack as VStack,
     __experimentalHStack as HStack,
+    __experimentalHeading as Heading,
 } from "@wordpress/components";
 
-import { commentAuthorAvatar } from "@wordpress/icons";
+import { commentAuthorAvatar, media } from "@wordpress/icons";
 
 
 export default function Form() {
@@ -35,14 +39,41 @@ export default function Form() {
                 value=""
             />
             <VStack>
-                <Heading align="" level={5}>Profile</Heading>
+                <Heading level={5}>Profile</Heading>
                 <HStack alignment="left">
                     <Icon icon={commentAuthorAvatar} size={75}></Icon>
-                    <Button variant="secondary">
-                        change
-                    </Button>
+                    <FormFileUpload
+                        onChange={function noRefCheck() { }}
+                        render={({ openFileDialog }) => <button onClick={openFileDialog}>Change</button>}
+                    >
+                        Select file
+                    </FormFileUpload>
                 </HStack>
+            </VStack>
+            <VStack>
+                <Heading level={5}>Cover Photo</Heading>
+                <Card variant="dotted">
+                    <CardBody>
+                        <VStack style={{ alignItems: 'center' }}>
+                            <Icon icon={media} style={{ fontSize: '48px' }} />
+                            <Text>
+                                <FormFileUpload accept="image/*" onChange={() => { }}>
+                                    Upload a file
+                                </FormFileUpload>
+                            </Text>
+                            <Text>or drag and drop PNG, JPG, GIF up to 10mb</Text>
+                        </VStack>
+                    </CardBody>
+                    <DropZone
+                        onDrop={function noRefCheck() { }}
+                        onFilesDrop={function noRefCheck() { }}
+                        onHTMLDrop={function noRefCheck() { }}
+                    />
+                </Card>
+
             </VStack>
         </VStack>
     );
 }
+
+
