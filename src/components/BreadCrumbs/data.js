fb271230350a2827fddd8802fd15a475e236d02d
Ajit Bohra <ajit@lubus.in>
2023-10-29 13:43:05 +0530

    fix code view for breadceumbs

--- src/components/BreadCrumbs/data.js
+++ src/components/BreadCrumbs/data.js
@@ -9,17 +9,17 @@ import * as Components from '.';
 const Breadcrumb = [
     {
         title: 'Breadcrumb 1',
-        path: '/Breadcrumbs/breadcrumb1',
+        path: '/BreadCrumbs/breadcrumb1',
         component: Components.Breadcrumb1
     },
     {
-        title: 'Breadcrumb 2',
-        path: '/Breadcrumbs/breadcrumb2',
+        title: 'Breadrumb 2',
+        path: '/BreadCrumbs/breadcrumb2',
         component: Components.Breadcrumb2
     },
     {
         title: 'Breadcrumb 3',
-        path: '/Breadcrumbs/breadcrumb3',
+        path: '/BreadCrumbs/breadcrumb3',
         component: Components.Breadcrumb3
     }
 ];
