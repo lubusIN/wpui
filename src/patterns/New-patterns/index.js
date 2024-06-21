f3c58a7f9fa6d514c27aebb73e52856b81196a05
Deepak Borana <djdeepakborana@gmail.com>
2024-06-21 16:21:59 +0530

    add(pattern): action panel

--- src/patterns/New-patterns/index.js
+++ src/patterns/New-patterns/index.js
@@ -5,5 +5,6 @@ export { default as Welcomescreen } from './welcomescreen';
 export { default as Actionpanel1 } from './actionpanel1';
 export { default as Actionpanel2 } from './actionpanel2';
 export { default as Actionpanel3 } from './actionpanel3';
+export { default as Actionpanel4 } from './actionpanel4';
 export { default as Card1 } from './card1';
 export { default as Emptystate1 } from './emptystate1';
