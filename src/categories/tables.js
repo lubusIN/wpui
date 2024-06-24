669f8814e1ef4f42eb2925d36278a5096e81e627
deepakgithub11 <167523195+deepakgithub11@users.noreply.github.com>
2024-06-24 14:57:21 +0530

    update: replace thumbnail images with svg components(#44)

---
+++ src/categories/tables.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Tables() {
+    return (
+        <svg width="307" height="230" viewBox="0 0 307 230" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="307" height="230" rx="6" fill="#F0F5FF" />
+            <rect x="13.5" y="51.5" width="280" height="161" rx="6.1246" stroke="#B8CFFF" />
+            <rect x="28" y="61" width="20" height="6" rx="1.89362" fill="#D6E4FF" />
+            <rect x="40" y="82" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="40" y="111" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="40" y="140" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="40" y="169" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="93" y="61" width="31" height="6" rx="1.89362" fill="#D6E4FF" />
+            <rect x="93" y="82" width="31" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="93" y="111" width="31" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="93" y="140" width="31" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="93" y="169" width="31" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="147" y="61" width="33" height="6" rx="1.89362" fill="#D6E4FF" />
+            <rect x="147" y="82" width="33" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="147" y="111" width="33" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="147" y="140" width="33" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="147" y="169" width="33" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="203" y="61" width="30" height="6" rx="1.89362" fill="#D6E4FF" />
+            <rect x="203" y="82" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="203" y="111" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="203" y="140" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <rect x="203" y="169" width="30" height="8" rx="1.89362" fill="#B8CFFF" />
+            <path d="M0 6C0 2.68629 2.68629 0 6 0H301C304.314 0 307 2.68629 307 6V33.4545H0V6Z" fill="#B8CFFF" />
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M16.7455 20.9091C19.0575 20.9091 20.9318 19.0368 20.9318 16.7273C20.9318 14.4177 19.0575 12.5455 16.7455 12.5455C14.4334 12.5455 12.5591 14.4177 12.5591 16.7273C12.5591 19.0368 14.4334 20.9091 16.7455 20.9091ZM30.7 20.9091C33.0121 20.9091 34.8864 19.0368 34.8864 16.7273C34.8864 14.4177 33.0121 12.5455 30.7 12.5455C28.3879 12.5455 26.5136 14.4177 26.5136 16.7273C26.5136 19.0368 28.3879 20.9091 30.7 20.9091ZM48.8409 16.7273C48.8409 19.0368 46.9666 20.9091 44.6546 20.9091C42.3425 20.9091 40.4682 19.0368 40.4682 16.7273C40.4682 14.4177 42.3425 12.5455 44.6546 12.5455C46.9666 12.5455 48.8409 14.4177 48.8409 16.7273Z" fill="white" />
+            <line x1="28" y1="72.5389" x2="284" y2="72.5389" stroke="white" stroke-width="0.92226" />
+            <line x1="25" y1="100.539" x2="281" y2="100.539" stroke="white" stroke-width="0.92226" />
+            <line x1="25" y1="130.539" x2="281" y2="130.539" stroke="white" stroke-width="0.92226" />
+            <line x1="25" y1="160.539" x2="281" y2="160.539" stroke="white" stroke-width="0.92226" />
+            <rect x="256" y="82" width="27" height="8" rx="4" fill="#3858E9" />
+            <rect x="256" y="111" width="27" height="8" rx="4" fill="#3858E9" />
+            <rect x="256" y="140" width="27" height="8" rx="4" fill="#3858E9" />
+            <rect x="256" y="169" width="27" height="8" rx="4" fill="#3858E9" />
+        </svg>
+    )
+}
+
+export default Tables;
