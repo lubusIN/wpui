09569a952786d2d59e3b8d9e000427b647570906
Ajit Bohra <ajit@codewalnut.com>
2024-06-24 21:16:38 +0530

    chore: fix svg component warnings

--- src/patterns/PluginShell/shell3.js
+++ src/patterns/PluginShell/shell3.js
@@ -79,7 +79,7 @@ function Shell3() {
                     </Navigation>
                 </HStack>
                 <HStack alignment="right">
-                    <Icon icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="white" stroke-width="2.5"><path stroke-linecap="round" stroke-linejoin="round" d="M6 19v-9a6 6 0 0 1 6-6v0a6 6 0 0 1 6 6v9M6 19h12M6 19H4m14 0h2m-9 3h2" /><circle cx="12" cy="3" r="1" /></g></svg>} size={25} ></Icon>
+                    <Icon icon={<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="white" strokeWidth="2.5"><path strokeLinecap="round" strokeLinejoin="round" d="M6 19v-9a6 6 0 0 1 6-6v0a6 6 0 0 1 6 6v9M6 19h12M6 19H4m14 0h2m-9 3h2" /><circle cx="12" cy="3" r="1" /></g></svg>} size={25} ></Icon>
                     <Icon fill="white" icon={commentAuthorAvatar} size={35}></Icon>
                 </HStack>
             </CardHeader>
