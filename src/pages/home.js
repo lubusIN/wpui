5c116a2d6c517ebb321f7561ebc739b022ee65b6
Deepak Borana <djdeepakborana@gmail.com>
2024-07-01 21:32:22 +0530

    refractor(heading): heading aligned

--- src/pages/home.js
+++ src/pages/home.js
@@ -22,7 +22,7 @@ function Home() {
         <>
             <Banner />
             <VStack className="wpui-lcont" spacing={5}>
-                <Heading align="center">UI Patterns</Heading>
+                <Heading className="wpui-heading">UI Patterns</Heading>
                 <Grid
                     className="wpui-column"
                     alignment="bottom"
