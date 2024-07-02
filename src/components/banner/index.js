6a7f5def22b36cfaf55ad185449f6d8191ebf330
Deepak Borana <167523195+deepakgithub11@users.noreply.github.com>
2024-07-02 10:28:49 +0530

    refactor(site): align text with cards (#65)
    * refractor(home page): UI pattern is alligned to UI cards
    
    * refactor(home page): Changes done in components texts
    
    * refractor(heading): heading aligned
    
    ---------
    
    Co-authored-by: Ajit Bohra <ajit@lubus.in>

--- src/components/banner/index.js
+++ src/components/banner/index.js
@@ -30,12 +30,12 @@ function Banner() {
             <CardBody className="wpui-hero-cont">
                 <VStack spacing={12}>
                     <VStack spacing={5}>
-                        <Text size={15} align="left" color="white">From the House of LUBUS</Text>
+                        <Text size={15} align="left" color="white">From the house of LUBUS</Text>
                         <Heading size={40} align="left" color="white" lineHeight={1.3} weight={500}>
                             Build Your React Powered WordPress Plugin Or App With Speed.
                         </Heading>
                         <Text size={16} align="left" color="white" lineHeight={1.5}>
-                            WPUI IS A DESIGN PATTERN LIBRARY BUILT UPON WORDPRESS COMPONENTS WHICH GIVES YOU THE BUILDING BLOCKS YOU NEED TO BUILD YOUR REACT-POWERED WORDPRESS PLUGIN OR APP. SPEND LESS TIME ON UI AND MORE TIME BUILDING YOUR IDEA.
+                        WPUI is a design pattern library built upon WordPress components which gives you the building blocks you need to build your react-powered WordPress plugin or app. Spend less time on UI and more time building your idea.
                         </Text>
                     </VStack>
                     <HStack className="wpui-hero-cta" alignment="center" justify="left">
@@ -45,14 +45,14 @@ function Banner() {
                             iconSize={20}
                             href='https://github.com/lubusIN/wpui/discussions'
                         >
-                            Join Discussion
+                            Join discussion
                         </Button>
                         <Link to="getting-started">
                         <Button
                             style={{ border: '1.5px solid #ffffff'}}
                             iconSize={20}
                         >
-                            Getting Started
+                            Getting started
                         </Button>
                         </Link>
                     </HStack>
