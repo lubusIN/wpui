e63b7bc492ff2d44ee3730bde87d984306e16e5c
Deepak Borana <djdeepakborana@gmail.com>
2024-07-09 16:52:47 +0530

    "feat(pattern-preview): resizeable box"

--- src/components/pattern-view/index.js
+++ src/components/pattern-view/index.js
@@ -7,13 +7,13 @@ import { useState } from 'react';
  * WordPress dependencies.
  */
 import { __ } from '@wordpress/i18n';
-
 import {
     code,
     seen
 } from "@wordpress/icons";
 import {
     Card,
+    ResizableBox,
     __experimentalHeading as Heading,
     __experimentalToggleGroupControl as ToggleGroupControl,
     __experimentalToggleGroupControlOptionIcon as ToggleGroupControlOptionIcon,
@@ -29,40 +29,45 @@ import './style.scss'
 
 /**
  * Render Pattern View
- */
+*/
 function PatternView({ title, path, component: Pattern }) {
     const [view, setView] = useState('preview');
-
     return (
-        <VStack spacing={4}>
-            <HStack>
-                <Heading level={4} weight={500}>{title}</Heading>
-                <HStack expanded={false} justify='right' alignment='center'>
-                    <ToggleGroupControl
-                        className="wpui-view-toggle"
-                        hideLabelFromVision
-                        __nextHasNoMarginBottom
-                        isBlock
-                        value={view}
-                        onChange={(value) => setView(value)}
-                    >
-                        <ToggleGroupControlOptionIcon icon={seen} value="preview" label="Preview" />
-                        <ToggleGroupControlOptionIcon icon={code} value="code" label="Code" />
-                    </ToggleGroupControl>
+        <>
+            <VStack spacing={4}>
+                <HStack>
+                    <Heading level={4} weight={500}>{title}</Heading>
+                    <HStack expanded={false} justify='right' alignment='center'>
+                        <ToggleGroupControl
+                            className="wpui-view-toggle"
+                            hideLabelFromVision
+                            __nextHasNoMarginBottom
+                            isBlock
+                            value={view}
+                            onChange={(value) => setView(value)}
+                        >
+                            <ToggleGroupControlOptionIcon icon={seen} value="preview" label="Preview" />
+                            <ToggleGroupControlOptionIcon icon={code} value="code" label="Code" />
+                        </ToggleGroupControl>
+                    </HStack>
                 </HStack>
-            </HStack>
+                {
+                    view === 'preview' && (
 
-            {
-                view === 'preview' && (
-                    <Card className="wpui-variation-card">
-                        <Pattern />
-                    </Card>
-                )
-            }
-
-            {view === 'code' && <PatternCode path={path} />}
-        </VStack>
+                        <ResizableBox
+                            enable={{ right: true }}
+                        >
+                            <Card className="wpui-variation-card">
+                                <Pattern />
+                            </Card>
+                        </ResizableBox>
+                    )
+                }
+                {view === 'code' && <PatternCode path={path} />}
+            </VStack>
+        </>
     );
 }
+
 
 export default PatternView;
