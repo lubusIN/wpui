81bb6f8171daa5deb6fb9e30d8a83441f7a8fc3a
Deepak Borana <djdeepakborana@gmail.com>
2024-08-16 11:09:36 +0530

    fix(pattern): shell responsive

--- src/patterns/shells/shell2.js
+++ src/patterns/shells/shell2.js
@@ -1,94 +1,89 @@
-/**
- * WordPress dependencies.
- */
+import React, { useState } from 'react';
 import {
     Card,
     CardHeader,
     CardBody,
     Button,
     __experimentalHStack as HStack,
+    __experimentalVStack as VStack,
     __experimentalHeading as Heading,
-    ToolbarGroup,
-    ToolbarItem
 } from "@wordpress/components";
-import { home, cog, plugins, key, external, trendingUp, menu } from "@wordpress/icons";
+import { home, cog, plugins, key, external, trendingUp, menu,close } from "@wordpress/icons";
 import { useViewportMatch } from '@wordpress/compose';
+
+function Files() {
+    return (
+        <VStack style={{padding:'12px'}} expanded={false} spacing={5} className='btn'>
+            <Button icon={home}>Dashboard</Button>
+            <Button icon={cog}>Settings</Button>
+            <Button icon={plugins}>Addons</Button>
+            <Button icon={key}>License</Button>
+            <Button icon={external}>Support</Button>
+            <Button style={{ display: 'flex', justifyContent: 'center' }} variant="primary" icon={trendingUp}>Gro Pro</Button>
+        </VStack>
+    );
+}
 
 /**
  * Render Subscription Shell
  */
 function Shell2() {
-    const isMobile = useViewportMatch('mobile');
+    const isMobile = !useViewportMatch('mobile');
+    const [showFiles, setShowFiles] = useState(false);
+    const [display , setDisplay]= useState('flex');
+    const [icon, setIcon] = useState(menu);
+
+    const handleButtonClick = () => {
+        if (icon === menu) {
+            setIcon(close);
+            setShowFiles(true);
+            setDisplay('none')
+        } else {
+            setIcon(menu);
+            setShowFiles(false);
+            setDisplay('flex')
+        }
+    };
+
     const desktop = (
         <>
-                <CardHeader >
-                    <HStack>
-                        <Heading size={20}>WPUI</Heading>
-                        <HStack expanded={false} spacing={5} className='btn'>
-                            <Button icon={home}>Dashboard</Button>
-                            <Button icon={cog}>Settings</Button>
-                            <Button icon={plugins}>Addons</Button>
-                            <Button icon={key}>License</Button>
-                            <Button icon={external}>Support</Button>
-                        </HStack>
-                        <Button variant="primary" icon={trendingUp}>Gro Pro</Button>
+            <CardHeader>
+                <HStack>
+                    <Heading size={20}>WPUI</Heading>
+                    <HStack expanded={false} spacing={5} className='btn'>
+                        <Button icon={home}>Dashboard</Button>
+                        <Button icon={cog}>Settings</Button>
+                        <Button icon={plugins}>Addons</Button>
+                        <Button icon={key}>License</Button>
+                        <Button icon={external}>Support</Button>
                     </HStack>
-                </CardHeader>
-                <CardBody style={{ padding: "100px",display:'flex',justifyContent:'center' }}>
-                    {/*Display Your Content Here*/}This is Output Panel
-                </CardBody>
+                    <Button variant="primary" icon={trendingUp}>Gro Pro</Button>
+                </HStack>
+            </CardHeader>
         </>
-    )
+    );
+
     const mobile = (
         <>
-                <CardHeader >
-                    <HStack>
-                        <Heading size={20}>WPUI</Heading>
-                        <ToolbarGroup
-                        style={{width:'100%'}}
-                            controls={[
-                                {
-                                    icon: home,
-                                    title: 'Dashboard'
-                                },
-                                {
-                                    icon: cog,
-                                    title: 'Settings'
-                                },
-                                {
-                                    icon: plugins,
-                                    title: 'Addons'
-                                },
-                                {
-                                    icon: key,
-                                    title: 'License'
-                                },
-                                {
-                                    icon: external,
-                                    title: 'Support'
-                                },
-                            ]}
-                            isCollapsed
-                            title="Align"
-                        />
-                        <ToolbarItem />
-                    </HStack>
-                </CardHeader>
-                <CardBody style={{ padding: "100px" ,display:'flex',justifyContent:'center'}}>
-                    {/*Display Your Content Here*/}This is Output Panel
-                </CardBody>
-                <Button variant="primary" icon={trendingUp} style={{width:'100%', justifyContent:'center'}}>Gro Pro</Button>
+            <CardHeader>
+                <HStack>
+                    <Heading size={20}>WPUI</Heading>
+                    <Button icon={icon} onClick={handleButtonClick}></Button>
+                </HStack>
+            </CardHeader>
+            {showFiles && <Files />}
         </>
-    )
-    return (
-<Card>
-    {isMobile ? desktop : mobile}
-</Card>
+    );
 
+    return (
+        <Card>
+            {isMobile ? mobile : desktop}
+            <CardBody style={{ padding: "100px", display: 'flex', justifyContent: 'center',display:display}}>
+                {/*Display Your Content Here*/}This is Output Panel
+            </CardBody>
+        </Card>
     );
 }
-
-
 
 // @meta-start
 Shell2.meta = {
