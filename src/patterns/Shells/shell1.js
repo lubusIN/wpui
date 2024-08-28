11aa4ad7d70744897fe6b1cf5bedf8258d348152
Deepak Borana <djdeepakborana@gmail.com>
2024-08-28 13:59:21 +0530

    remove card from shell 1

--- src/patterns/Shells/shell1.js
+++ src/patterns/Shells/shell1.js
@@ -36,54 +36,50 @@ function Shell1() {
     ];
     return (
         <>
-            <Card>
-                <CardHeader>
-                    <HStack>
-                        <img width='100px' style={{ minWidth: 'auto' }} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/logo.png"></img>
-                        {isMobile ? (
-                            <Button icon={showButtons ? close : menu} onClick={() => setShowButtons(prev => !prev)}></Button>
-                        ) : (
-                            <>
-                                <HStack expanded={false}>
-                                    {buttonData.slice(0, -1).map((btn, index) => (
-                                        <Button key={index} >
-                                            <Icon style={{ minWidth: '20px' }} icon={btn.icon} />
-                                            {btn.label}
-                                        </Button>
-                                    ))}
-                                </HStack>
-                                <Button variant="primary" icon={trendingUp}>Go Pro</Button>
-                            </>
-                        )}
-                    </HStack>
-                </CardHeader>
-                <ZStack isReversed style={{ width: '100%' }}>
-                    {isMobile && showButtons && (
-                        <VStack style={{ padding: '12px', backgroundColor: 'white' }} expanded={false}>
-                            {buttonData.map((btn, index) => (
-                                <Button
-                                    key={index}
-                                    icon={btn.icon}
-                                    variant={btn.variant}
-                                    style={btn.label === 'Go Pro' ? { justifyContent: 'center' } : {}}
-                                >
+            <HStack expanded={false} style={{padding:'12px 20px'}}>
+                <img width='100px' style={{ minWidth: 'auto' }} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/logo.png"></img>
+                {isMobile ? (
+                    <Button icon={showButtons ? close : menu} onClick={() => setShowButtons(prev => !prev)}></Button>
+                ) : (
+                    <>
+                        <HStack expanded={false}>
+                            {buttonData.slice(0, -1).map((btn, index) => (
+                                <Button key={index} >
+                                    <Icon style={{ minWidth: '25px' }} icon={btn.icon} />
                                     {btn.label}
                                 </Button>
                             ))}
-                        </VStack>
-                    )}
-                    <Card size="medium" variant="secondary" isBorderless>
-                        <CardHeader isBorderless>
-                            <Heading level={2}>Dashboard</Heading>
-                        </CardHeader>
-                        <CardBody>
-                            <Card variant="secondary" style={{ height: '300px', borderRadius: '10px' }}>
-                                {/* Display Your Content Here */}
-                            </Card>
-                        </CardBody>
-                    </Card>
-                </ZStack>
-            </Card>
+                        </HStack>
+                        <Button variant="primary" icon={trendingUp}>Go Pro</Button>
+                    </>
+                )}
+            </HStack>
+            <ZStack isReversed style={{ width: '100%' }}>
+                {isMobile && showButtons && (
+                    <VStack style={{ padding: '12px', backgroundColor: 'white' }} expanded={false}>
+                        {buttonData.map((btn, index) => (
+                            <Button
+                                key={index}
+                                icon={btn.icon}
+                                variant={btn.variant}
+                                style={btn.label === 'Go Pro' ? { justifyContent: 'center' } : {}}
+                            >
+                                {btn.label}
+                            </Button>
+                        ))}
+                    </VStack>
+                )}
+                <Card variant="secondary" isBorderless>
+                    <CardHeader isBorderless>
+                        <Heading level={2}>Dashboard</Heading>
+                    </CardHeader>
+                    <CardBody>
+                        <Card variant="secondary" style={{ height: '300px', borderRadius: '10px' }}>
+                            {/* Display Your Content Here */}
+                        </Card>
+                    </CardBody>
+                </Card>
+            </ZStack>
             <style>
                 {`
                     .components-z-stack >div{
