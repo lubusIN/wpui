4db03bd4528143692d1d651f6f8b2d2d0e5dc8d1
Deepak Borana <djdeepakborana@gmail.com>
2024-08-29 12:42:21 +0530

    refactor code

--- src/patterns/Shells/shell3.js
+++ src/patterns/Shells/shell3.js
@@ -63,116 +63,66 @@ function Shell3() {
     };
 
     return (
-        <>
-            <Card>
-                <CardHeader>
-                    <HStack>
-                        <img width='100px' style={{ minWidth: 'auto' }} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/logo.png"></img>
-                        <HStack alignment="right">
-                            <Button variant="primary">Publish</Button>
-                            <Button icon={drawerRight} onClick={() => setBlock(!block)} />
-                            <DropdownMenu icon={moreVertical}>
-                                {() => (
-                                    <MenuGroup>
-                                        <MenuItem>About us</MenuItem>
-                                        <MenuItem>Contact us</MenuItem>
-                                        <MenuItem>More Info</MenuItem>
-                                    </MenuGroup>
-                                )}
-                            </DropdownMenu>
-                        </HStack>
+        <Card>
+            <CardHeader>
+                <HStack>
+                    <img width='100px' style={{ minWidth: 'auto' }} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/logo.png"></img>
+                    <HStack alignment="right">
+                        <Button variant="primary">Publish</Button>
+                        <Button icon={drawerRight} onClick={() => setBlock(!block)} />
+                        <DropdownMenu icon={moreVertical}>
+                            {() => (
+                                <MenuGroup>
+                                    <MenuItem>About us</MenuItem>
+                                    <MenuItem>Contact us</MenuItem>
+                                    <MenuItem>More Info</MenuItem>
+                                </MenuGroup>
+                            )}
+                        </DropdownMenu>
                     </HStack>
-                </CardHeader>
-                <HStack spacing={0}>
-                    {isMobile ? (
-                        <>
-                            <ZStack style={{ width: '100%' }}>
-                                <CardBody style={{ padding: '0', height: '500px', width: '100%'}}>
-                                    {/* Display Your Content Here */}
-                                    <iframe height='100%' width="100%" src="https://playground.wordpress.net/"></iframe>
-                                </CardBody>
-                                <CardBody
-                                    style={{
-                                        height: '720px',
-                                        padding: '0px',
-                                        width:'100%',
-                                        padding:'10px'
-                                    }}
-                                >
-                                    {block && (
-                                        <Animate type="slide-in" options={{ origin: 'left' }}>
-                                            {({ className }) => (
-                                                <div className={className} style={{ height: '100%', width: '100%' }}>
-                                                    <TabPanel
-                                                        tabs={[
-                                                            { name: 'tab1', title: 'Tab 1' },
-                                                            { name: 'tab2', title: 'Tab 2' },
-                                                        ]}
-                                                    >
-                                                        {({ name }) => (
-                                                            <div style={{ height: '320px', width: '94vw', overflowY: 'hidden' }}>
-                                                                {tabsContent[name]}
-                                                            </div>
-                                                        )}
-                                                    </TabPanel>
+                </HStack>
+            </CardHeader>
+            <HStack spacing={0}>
+                <>
+                    <CardBody style={{
+                        padding: '0', height: '500px', width: '100%'
+                    }}>
+                        {/* Display Your Content Here */}
+                    </CardBody>
+                    <CardBody
+                        style={{
+                            borderLeft: '1px solid #dfdfdf',
+                            height: '720px',
+                            width: '280px',
+                            overflow: 'hidden',
+                            padding: '0px'
+                        }}
+                    >
+                        {block && (
+                            <Animate type="slide-in" options={{ origin: 'left' }}>
+                                {({ className }) => (
+                                    <div className={className} style={{ height: '100%', width: 'auto' }}>
+                                        <TabPanel
+                                            tabs={[
+                                                { name: 'tab1', title: 'Tab 1' },
+                                                { name: 'tab2', title: 'Tab 2' },
+                                            ]}
+                                        >
+                                            {({ name }) => (
+                                                <div style={{ padding: '10px', height: '320px', width: 'auto' }}>
+                                                    {tabsContent[name]}
                                                 </div>
                                             )}
-                                        </Animate>
-                                    )}
-                                </CardBody>
-                            </ZStack>
-                        </>
-                    ) : (
-                        <>
-                            <CardBody style={{ padding: '0', height: '500px', width: '100%'
-                            }}>
-                                {/* Display Your Content Here */}
-                                <iframe height='100%' width="100%" src="https://playground.wordpress.net/"></iframe>
-                            </CardBody>
-                            <CardBody
-                                style={{
-                                    borderLeft: '1px solid #dfdfdf',
-                                    height: '720px',
-                                    width:'280px',
-                                    overflow: 'hidden',
-                                    padding: '0px'
-                                }}
-                            >
-                                {block && (
-                                    <Animate type="slide-in" options={{ origin: 'left' }}>
-                                        {({ className }) => (
-                                            <div className={className} style={{ height: '100%', width: 'auto' }}>
-                                                <TabPanel
-                                                    tabs={[
-                                                        { name: 'tab1', title: 'Tab 1' },
-                                                        { name: 'tab2', title: 'Tab 2' },
-                                                    ]}
-                                                >
-                                                    {({ name }) => (
-                                                        <div style={{ padding: '10px', height: '320px', width: 'auto', overflowY: 'hidden' }}>
-                                                            {tabsContent[name]}
-                                                        </div>
-                                                    )}
-                                                </TabPanel>
-                                            </div>
-                                        )}
-                                    </Animate>
+                                        </TabPanel>
+                                    </div>
                                 )}
-                            </CardBody>
-                        </>
-                    )}
+                            </Animate>
+                        )}
+                    </CardBody>
+                </>
 
-                </HStack>
-            </Card>
-
-            <style>
-                {`
-    .components-z-stack >div{
-        width: 100%;
-    }
-`}
-            </style>
-        </>
+            </HStack>
+        </Card>
 
     );
 };
