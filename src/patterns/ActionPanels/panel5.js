823255d2be765bab01f50a7a5c27df6e599e6c53
Deepak Borana <djdeepakborana@gmail.com>
2024-08-29 15:21:21 +0530

    add action-panel-5

---
+++ src/patterns/ActionPanels/panel5.js
@@ -0,0 +1,79 @@
+/**
+ * WordPress dependencies.
+ */
+import {
+    Card,
+    CardBody,
+    Icon,
+    Button,
+    __experimentalHeading as Heading,
+    __experimentalText as Text,
+    __experimentalVStack as VStack,
+    __experimentalHStack as HStack,
+} from "@wordpress/components";
+import { trendingUp, megaphone, archive } from "@wordpress/icons";
+
+/**
+ * Render Subscription action panel
+ */
+function Panel5() {
+    return (
+        <>
+            <HStack alignment="center">
+                <Card size="large" style={{ borderRadius: '8px' }}>
+                    <HStack alignment="top">
+                        <CardBody>
+                            <VStack spacing={2}>
+                                <Heading level={1}>
+                                    Reach  new customers and increase sales
+                                </Heading>
+                                <VStack spacing={3}>
+                                    <HStack expanded={false} alignment="left">
+                                        <Icon size={20} icon={trendingUp}></Icon>
+                                        <Text variant="muted">Reach customers on other sale channels.</Text>
+                                    </HStack>
+                                    <HStack expanded={false} alignment="left">
+                                        <Icon size={20} icon={megaphone}></Icon>
+                                        <Text variant="muted">Advertise with marketing campaigns.</Text>
+                                    </HStack>
+                                    <HStack expanded={false} alignment="left">
+                                        <Icon size={20} icon={archive}></Icon>
+                                        <Text variant="muted">Build with Us.</Text>
+                                    </HStack>
+                                    <HStack expanded={false} alignment="left">
+                                        <Button variant="primary">Create a campaigns</Button>
+                                        <Button variant="secondary">Add channels</Button>
+                                    </HStack>
+                                </VStack>
+                            </VStack>
+                        </CardBody>
+                        <CardBody>
+                            <VStack>
+                                <img height='100px' width='250px' src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar2.png"></img>
+                            </VStack>
+                                <img id="small" height='200px' width='250px' src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar1.png"></img>
+                        </CardBody>
+                    </HStack>
+                </Card>
+            </HStack>
+            <style>
+                {`
+                .small{
+                margin-right:20px !important;
+                }
+                `}
+            </style>
+        </>
+    );
+};
+
+// @meta-start
+Panel5.meta = {
+    title: 'Panel 5',
+    name: 'Panel5',
+    category: 'ActionPanels',
+    path: '/ActionPanels/panel5',
+};
+// @meta-end
+
+export default Panel5;
