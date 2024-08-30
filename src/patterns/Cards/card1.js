fbc27736b39431f5d8777e3f677bdad9c4107c72
Deepak Borana <djdeepakborana@gmail.com>
2024-08-30 15:56:31 +0530

    add/cards

--- src/patterns/Cards/card1.js
+++ src/patterns/Cards/card1.js
@@ -11,32 +11,34 @@ import {
     __experimentalVStack as VStack,
     CardBody,
 } from "@wordpress/components";
-import { arrowRight,starEmpty } from "@wordpress/icons";
+import { arrowRight, starEmpty } from "@wordpress/icons";
 
 /**
  * Render Card.
  */
 function Card1() {
     return (
-        <Card size="medium" style={{ borderRadius: '4px' }}>
-            <CardBody>
-            <HStack alignment='top'>
-                <Icon icon={starEmpty} size={25}></Icon>
-                <VStack>
-                    <Heading>Build WP Plugin Faster</Heading>
-                    <Text variant='muted' style={{ maxWidth: '400px' }} >
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-                    </Text>
-                    <Button style={{ padding: 0 }}>
-                        <Text size={14} weight={600} as={"a"} href="https://wordpress.org" color='blue' style={{ textDecoration: 'none' }} >
-                            Link Text
-                        </Text>
-                        <Icon icon={arrowRight} color="blue"></Icon>
-                    </Button>
-                </VStack>
-            </HStack>
-            </CardBody>
-        </Card>
+        <HStack alignment="center">
+            <Card size="large" style={{ borderRadius: '4px' }}>
+                <CardBody>
+                    <HStack alignment='top'>
+                        <Icon style={{ minWidth: '25px' }} icon={starEmpty} size={25}></Icon>
+                        <VStack>
+                            <Heading level={4} >Build WP Plugin Faster</Heading>
+                            <Text variant='muted' style={{ maxWidth: '225px' }} >
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                            </Text>
+                            <Button style={{ padding: 0 }}>
+                                <Text size={14} weight={600} color='#3858E9' style={{ textDecoration: 'none' }} >
+                                    Link Text
+                                </Text>
+                                <Icon icon={arrowRight} color="#3858E9"></Icon>
+                            </Button>
+                        </VStack>
+                    </HStack>
+                </CardBody>
+            </Card >
+        </HStack>
     );
 };
 
