9c58b21fbd89276006d34394eccf30df4f9a4bc7
Deepak Borana <djdeepakborana@gmail.com>
2024-09-02 13:01:07 +0530

    update: action-panel-1

--- src/patterns/ActionPanels/panel1.js
+++ src/patterns/ActionPanels/panel1.js
@@ -19,16 +19,14 @@ function Panel1() {
         <HStack alignment="center">
             <Card size="large" style={{ borderRadius: '8px' }}>
                 <CardBody>
-                    <HStack spacing={8}>
-                        <VStack spacing={6} alignment="baseline">
-                            <VStack spacing={3}>
-                                <Heading>Lorem ipsum</Heading>
-                                <Text size={15} weight={400} variant="muted" style={{maxWidth:'600px'}}>
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae voluptatibus corrupti atque repudiandae nam.
-                                </Text>
-                            </VStack>
+                    <HStack direction={['column', 'row']} alignment={'start'} justify='left' >
+                        <VStack spacing={3} alignment="baseline">
+                            <Heading level={3}>Update Your Profile</Heading>
+                            <Text size={15} weight={400} variant="muted" style={{ maxWidth: '600px' }}>
+                                Elevate your online presence by personalizing your user profile with the latest information.
+                            </Text>
                         </VStack>
-                        <Button variant="primary" style={{minWidth:'auto'}}>Call to action</Button>
+                        <Button variant="primary" style={{ minWidth: 'auto' }}>Update Profile</Button>
                     </HStack>
                 </CardBody>
             </Card>
@@ -39,8 +37,8 @@ function Panel1() {
 // @meta-start
 Panel1.meta = {
     title: 'Panel 1',
-    name: 'Panel1',
-    category: 'ActionPanels',
+    name:'Panel1',
+    category:'ActionPanels',
     path: '/ActionPanels/panel1',
 };
 // @meta-end
