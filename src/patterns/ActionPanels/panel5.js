9f28a36e16aa4143ebb35a694ccce0b42a91fd6a
Deepak Borana <djdeepakborana@gmail.com>
2024-09-02 15:29:35 +0530

    update: action-panel-5

--- src/patterns/ActionPanels/panel5.js
+++ src/patterns/ActionPanels/panel5.js
@@ -23,27 +23,35 @@ function Panel5() {
                 <HStack alignment="top">
                     <CardBody>
                         <VStack spacing={2}>
-                            <Heading level={1}>
-                                Reach  new customers and increase sales
-                            </Heading>
-                            <VStack spacing={3}>
-                                <HStack expanded={false} alignment="left">
-                                    <Icon size={20} icon={trendingUp}></Icon>
-                                    <Text variant="muted">Reach customers on other sale channels.</Text>
-                                </HStack>
-                                <HStack expanded={false} alignment="left">
-                                    <Icon size={20} icon={megaphone}></Icon>
-                                    <Text variant="muted">Advertise with marketing campaigns.</Text>
-                                </HStack>
-                                <HStack expanded={false} alignment="left">
-                                    <Icon size={20} icon={archive}></Icon>
-                                    <Text variant="muted">Build with Us.</Text>
-                                </HStack>
-                                <HStack expanded={false} alignment="left">
-                                    <Button variant="primary">Create a campaigns</Button>
-                                    <Button variant="secondary">Add channels</Button>
-                                </HStack>
-                            </VStack>
+                            <HStack alignment="top" style={{ width: '750px' }} spacing={40}>
+                                <Heading level={1}>
+                                    Reach  new customers and increase sales
+                                </Heading>
+                                <img src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar2.png"></img>
+                            </HStack>
+                            <HStack alignment="top" spacing={40} justify="left">
+                                <VStack spacing={8}>
+                                    <VStack spacing={3}>
+                                        <HStack expanded={false} alignment="left">
+                                            <Icon size={20} icon={trendingUp}></Icon>
+                                            <Text weight={500} variant="muted">Reach customers on other sale channels.</Text>
+                                        </HStack>
+                                        <HStack expanded={false} alignment="left">
+                                            <Icon size={20} icon={megaphone}></Icon>
+                                            <Text weight={500} variant="muted">Advertise with marketing campaigns.</Text>
+                                        </HStack>
+                                        <HStack expanded={false} alignment="left">
+                                            <Icon size={20} icon={archive}></Icon>
+                                            <Text weight={500} variant="muted">Build with Us.</Text>
+                                        </HStack>
+                                    </VStack>
+                                    <HStack expanded={false} alignment="left">
+                                        <Button variant="primary">Create a campaigns</Button>
+                                        <Button variant="secondary">getting started </Button>
+                                    </HStack>
+                                </VStack>
+                                <img height='200px' src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar1.png"></img>
+                            </HStack>
                         </VStack>
                     </CardBody>
                 </HStack>
