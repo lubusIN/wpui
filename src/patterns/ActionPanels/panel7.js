d5f008b1f2a9623ef9ba6e2c07fa30b6ea4c4d5f
Deepak Borana <djdeepakborana@gmail.com>
2024-09-02 13:56:37 +0530

    update: action-panel-7

--- src/patterns/ActionPanels/panel7.js
+++ src/patterns/ActionPanels/panel7.js
@@ -25,25 +25,24 @@ function Panel7() {
             <Card style={{ borderRadius: '8px' }}>
                 <CardHeader>
                     <HStack expanded={false} spacing={1}>
-                        <Icon icon={key}></Icon>
+                        <Icon icon={key} size={30}></Icon>
                         <Heading level={3}>Pro licensing</Heading>
                     </HStack>
                 </CardHeader>
                 <CardBody>
                     <VStack spacing={3}>
-                        <Text size={15} weight={600}>
+                        <Text size={15} weight={500}>
                             Enter your license here
                         </Text>
                         <HStack expanded={false} spacing={2} alignment='left'>
-                            <InputControl placeholder="Paste your license key here..." />
-                            <Button style={{ minWidth: 'auto' }} size='compact' variant='primary'>Activate</Button>
+                            <InputControl __unstableInputWidth={200} style={{ height: '36px' }} placeholder="Paste your license key here..." />
+                            <Button style={{ maxWidth: 'auto' }} variant='primary'>Activate</Button>
                         </HStack>
                         <Text size={15} weight={400} variant="muted">
                             If you do not have a license key, you can
-                            <ExternalLink href="" style={{ textDecoration: 'none', color: 'blue' }}> purchase one here </ExternalLink>
+                            <ExternalLink href="" style={{ textDecoration: 'none', color: '#3858E9' }}> purchase one here </ExternalLink>
                         </Text>
                     </VStack>
-
                 </CardBody>
             </Card>
         </HStack>
