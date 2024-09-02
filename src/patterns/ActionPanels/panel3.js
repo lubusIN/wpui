bbec9e65663dee1f0d904558675ccda48b033a65
Deepak Borana <djdeepakborana@gmail.com>
2024-09-02 16:00:22 +0530

    update : panel 3

--- src/patterns/ActionPanels/panel3.js
+++ src/patterns/ActionPanels/panel3.js
@@ -25,25 +25,27 @@ function Panel3() {
         <HStack alignment="center">
             <Card size="large" style={{ borderRadius: '8px' }}>
                 <CardBody>
-                    <Heading level={4}>Backup Your Website Automatically</Heading>
-                    <HStack direction={['column', 'row']} alignment={'start'} justify='left' >
-                        <VStack>
-                            <Text
-                                size={14}
-                                lineHeight={1.6}
-                                weight={400}
-                                style={{ maxWidth: '600px' }}
-                                variant="muted"
-                            >
-                                Automate the safeguarding of your valuable content with scheduled automatic backups, ensuring the continuous protection of your website data against unforeseen events.
-                            </Text>
-                        </VStack>
-                        <ToggleControl
-                            __nextHasNoMarginBottom
-                            checked={value}
-                            onChange={() => setValue((state) => !state)}
-                        />
-                    </HStack>
+                    <VStack spacing={2}>
+                        <Heading level={4}>Backup Your Website Automatically</Heading>
+                        <HStack direction={['column', 'row']} alignment={'start'} justify='left' >
+                            <VStack>
+                                <Text
+                                    size={14}
+                                    lineHeight={1.6}
+                                    weight={400}
+                                    style={{ maxWidth: '600px' }}
+                                    variant="muted"
+                                >
+                                    Automate the safeguarding of your valuable content with scheduled automatic backups, ensuring the continuous protection of your website data against unforeseen events.
+                                </Text>
+                            </VStack>
+                            <ToggleControl
+                                __nextHasNoMarginBottom
+                                checked={value}
+                                onChange={() => setValue((state) => !state)}
+                            />
+                        </HStack>
+                    </VStack>
                 </CardBody>
             </Card>
         </HStack >
