99f9494681dbb551d6fffc1c112c92f49b866d6a
Deepak Borana <djdeepakborana@gmail.com>
2024-09-02 16:43:33 +0530

    update action panel

--- src/patterns/ActionPanels/panel6.js
+++ src/patterns/ActionPanels/panel6.js
@@ -21,31 +21,39 @@ import { key } from "@wordpress/icons";
  */
 function Panel6() {
     return (
-        <HStack alignment="center">
-            <Card style={{ borderRadius: '8px' }}>
-                <CardHeader>
-                    <HStack expanded={false} spacing={1}>
-                        <Icon icon={key} size={30}></Icon>
-                        <Heading level={3}>Pro licensing</Heading>
-                    </HStack>
-                </CardHeader>
-                <CardBody>
-                    <VStack spacing={3}>
-                        <Text size={15} weight={500}>
-                            Enter your license here
-                        </Text>
-                        <HStack expanded={false} spacing={2} direction={['column', 'row']} alignment={'start'} justify='left'>
-                            <InputControl __unstableInputWidth={200} style={{ height: '36px' }} placeholder="Paste your license key here..." />
-                            <Button style={{ maxWidth: 'auto' }} variant='primary'>Activate</Button>
+        <>
+            <HStack alignment="center">
+                <Card style={{ borderRadius: '8px' }}>
+                    <CardHeader>
+                        <HStack expanded={false} spacing={1}>
+                            <Icon icon={key} size={30}></Icon>
+                            <Heading level={3}>Pro licensing</Heading>
                         </HStack>
-                        <Text size={15} weight={400} variant="muted">
-                            If you do not have a license key, you can
-                            <ExternalLink href="" style={{ textDecoration: 'none', color: '#3858E9' }}> purchase one here </ExternalLink>
-                        </Text>
-                    </VStack>
-                </CardBody>
-            </Card>
-        </HStack>
+                    </CardHeader>
+                    <CardBody>
+                        <VStack spacing={3}>
+                            <Text size={15} weight={500}>
+                                Enter your license here
+                            </Text>
+                            <HStack expanded={false} spacing={2} direction={['column', 'row']} alignment={'start'} justify='left'>
+                                <InputControl __unstableInputWidth={200} style={{ height: '36px' }} placeholder="Paste your license key here..." />
+                                <Button style={{ maxWidth: 'auto' }} variant='primary'>Activate</Button>
+                            </HStack>
+                            <Text size={15} weight={400} variant="muted">
+                                If you do not have a license key, you can
+                                <ExternalLink href="" style={{ textDecoration: 'none', color: '#3858E9' }}> purchase one here </ExternalLink>
+                            </Text>
+                        </VStack>
+                    </CardBody>
+                </Card>
+            </HStack>
+            <style>
+                {`
+                .components-input-control__container{
+                width:300px;}
+                `}
+            </style>
+        </>
     );
 };
 
