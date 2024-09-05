703092fc2e1d5519d816d6671d4a1bd31e6b32d9
Deepak Borana <djdeepakborana@gmail.com>
2024-09-05 11:02:13 +0530

    refactor code

--- src/patterns/ActionPanels/panel7.js
+++ src/patterns/ActionPanels/panel7.js
@@ -22,49 +22,47 @@ import { trendingUp, megaphone, archive } from "@wordpress/icons";
 function Panel7() {
     const isMobile = !useViewportMatch('mobile')
     return (
-        <>
-            <HStack alignment="center">
-                <Card size="large" style={{ borderRadius: '8px' }}>
-                    <CardBody>
-                        <HStack alignment="top" spacing={0}>
-                            <VStack spacing={2}>
-                                <HStack alignment='topLeft' style={{ maxWidth: '500px' }} >
-                                    <Heading level={1} >
-                                        Reach  new customers and increase sales
-                                    </Heading>
-                                </HStack>
-                                <HStack>
-                                    <VStack spacing={8}>
-                                        <VStack spacing={3}>
-                                            <HStack expanded={false} alignment="left" spacing={2}>
-                                                <Icon style={{ minWidth: '25px' }} icon={trendingUp}></Icon>
-                                                <Text weight={500} variant="muted">Reach customers on other sale channels.</Text>
-                                            </HStack>
-                                            <HStack expanded={false} alignment="left">
-                                                <Icon style={{ minWidth: '25px' }} icon={megaphone}></Icon>
-                                                <Text weight={500} variant="muted">Advertise with marketing campaigns.</Text>
-                                            </HStack>
-                                            <HStack expanded={false} alignment="left">
-                                                <Icon style={{ minWidth: '25px' }} icon={archive}></Icon>
-                                                <Text weight={500} variant="muted">Build with Us.</Text>
-                                            </HStack>
-                                        </VStack>
-                                        <Grid columns={[1, 2, 2]}>
-                                            <Button style={{ justifyContent: 'center' }} variant="primary">Create a campaigns</Button>
-                                            <Button style={{ justifyContent: 'center' }} variant='secondary'>Getting started</Button>
-                                        </Grid>
+        <HStack alignment="center">
+            <Card size="large" style={{ borderRadius: '8px' }}>
+                <CardBody>
+                    <HStack alignment="top" spacing={0}>
+                        <VStack spacing={2}>
+                            <HStack alignment='top' style={{ maxWidth: '500px' }} >
+                                <Heading level={1} >
+                                    Reach  new customers and increase sales
+                                </Heading>
+                            </HStack>
+                            <HStack>
+                                <VStack spacing={8}>
+                                    <VStack spacing={3}>
+                                        <HStack expanded={false} alignment="left" spacing={2}>
+                                            <Icon style={{ minWidth: '25px' }} icon={trendingUp}></Icon>
+                                            <Text weight={500} variant="muted">Reach customers on other sale channels.</Text>
+                                        </HStack>
+                                        <HStack expanded={false} alignment="left">
+                                            <Icon style={{ minWidth: '25px' }} icon={megaphone}></Icon>
+                                            <Text weight={500} variant="muted">Advertise with marketing campaigns.</Text>
+                                        </HStack>
+                                        <HStack expanded={false} alignment="left">
+                                            <Icon style={{ minWidth: '25px' }} icon={archive}></Icon>
+                                            <Text weight={500} variant="muted">Build with Us.</Text>
+                                        </HStack>
                                     </VStack>
-                                </HStack>
-                            </VStack>
-                            <VStack className='wpui_panel_container' style={{ display: isMobile ? 'none' : '' }} >
-                                <Surface as={'img'} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar2.png" style={{ height: '100%', width: '200px', marginLeft: '100px' }} />
-                                <Surface as={'img'} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar1.png" style={{ height: '100%', width: '200px' }} />
-                            </VStack>
-                        </HStack>
-                    </CardBody>
-                </Card>
-            </HStack>
-        </>
+                                    <Grid columns={[1, 2, 2]}>
+                                        <Button style={{ justifyContent: 'center' }} variant="primary">Create a campaigns</Button>
+                                        <Button style={{ justifyContent: 'center' }} variant='secondary'>Getting started</Button>
+                                    </Grid>
+                                </VStack>
+                            </HStack>
+                        </VStack>
+                        <VStack className='wpui_panel_container' style={{ display: isMobile ? 'none' : '' }} >
+                            <Surface as={'img'} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar2.png" style={{ height: '100%', width: '200px', marginLeft: '100px' }} />
+                            <Surface as={'img'} src="https://raw.githubusercontent.com/lubusIN/wpui/main/src/img/avtar/avtar1.png" style={{ height: '100%', width: '200px' }} />
+                        </VStack>
+                    </HStack>
+                </CardBody>
+            </Card>
+        </HStack>
     );
 };
 
