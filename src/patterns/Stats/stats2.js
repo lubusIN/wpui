549a53616b27ba352a97a97840c4b6d515efeb38
Deepak Borana <djdeepakborana@gmail.com>
2024-09-06 16:43:48 +0530

    refactor code

--- src/patterns/Stats/stats2.js
+++ src/patterns/Stats/stats2.js
@@ -23,9 +23,8 @@ function Stats2() {
     const cardData = [
         {
             title: 'Total Orders',
-            current: '71,987',
-            previous: '70,900',
-            percentage: '12%',
+            current: '11,204',
+            percentage: '5.0%',
             icon: arrowUp,
             iconColor: '#22C55D',
             textColor: '#176434',
@@ -33,9 +32,8 @@ function Stats2() {
         },
         {
             title: 'Avg. Order Value',
-            current: '₹50,000',
-            previous: '₹100,000',
-            percentage: '10%',
+            current: '78.80%',
+            percentage: '2.0%',
             icon: arrowUp,
             iconColor: '#22C55D',
             textColor: '#176434',
@@ -43,9 +41,8 @@ function Stats2() {
         },
         {
             title: 'Order Conversion Rate',
-            current: '24.57%',
-            previous: '28.62%',
-            percentage: '4.04%',
+            current: '12.43%',
+            percentage: '2.05%',
             icon: arrowDown,
             iconColor: '#B04A48',
             textColor: '#B04A48',
@@ -55,26 +52,22 @@ function Stats2() {
     return (
         <Card isBorderless isRounded>
             <VStack spacing={5}>
-                <Heading level={3}>Last 30 days</Heading>
+                <Heading level={3}>Last 7 days</Heading>
                 <Grid columns={[1, 2, 3]} gap={0}>
                     {cardData.map((data, index) => (
                         <Card key={index} style={{ padding: '20px' }}>
-                            <VStack spacing={3}>
+                            <VStack spacing={2}>
                                 <Heading level={5} weight={500}>{data.title}</Heading>
                                 <HStack>
-                                    <HStack justify="left" spacing={1}>
-                                        <Heading level={2} color="blue">{data.current}</Heading>
-                                        <Text>from {data.previous}</Text>
-                                    </HStack>
+                                    <Heading level={2} color="#3858E9">{data.current}</Heading>
                                     <Surface
                                         style={{
                                             backgroundColor: data.backgroundColor,
-                                            borderRadius: '10px',
                                             padding: '0px 15px 0px 5px'
                                         }}>
                                         <HStack spacing={0}>
                                             <Icon icon={data.icon} fill={data.iconColor} />
-                                            <Text color={data.textColor}>{data.percentage}</Text>
+                                            <Text weight={500} color={data.textColor}>{data.percentage}</Text>
                                         </HStack>
                                     </Surface>
                                 </HStack>
