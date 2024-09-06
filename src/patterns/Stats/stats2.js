816ed0af4a969599ff20f47edb0387ebda038170
Deepak Borana <djdeepakborana@gmail.com>
2024-09-06 14:20:26 +0530

    update: stats1

--- src/patterns/Stats/stats2.js
+++ src/patterns/Stats/stats2.js
@@ -1,14 +1,9 @@
 /**
  * WordPress dependencies.
  */
-import { 
-    arrowUp, 
-    arrowDown 
-} from "@wordpress/icons";
 import {
     Card,
     Icon,
-    CardBody,
     __experimentalGrid as Grid,
     __experimentalText as Text,
     __experimentalHeading as Heading,
@@ -16,87 +11,76 @@ import {
     __experimentalVStack as VStack,
     __experimentalSurface as Surface
 } from "@wordpress/components";
+import {
+    arrowUp,
+    arrowDown
+} from "@wordpress/icons";
 
 /**
  * Render Stats
  */
 function Stats2() {
+    const cardData = [
+        {
+            title: 'Total Orders',
+            current: '71,987',
+            previous: '70,900',
+            percentage: '12%',
+            icon: arrowUp,
+            iconColor: '#22C55D',
+            textColor: '#176434',
+            backgroundColor: '#DCFCE6'
+        },
+        {
+            title: 'Avg. Order Value',
+            current: '₹50,000',
+            previous: '₹100,000',
+            percentage: '10%',
+            icon: arrowUp,
+            iconColor: '#22C55D',
+            textColor: '#176434',
+            backgroundColor: '#DCFCE6'
+        },
+        {
+            title: 'Order Conversion Rate',
+            current: '24.57%',
+            previous: '28.62%',
+            percentage: '4.04%',
+            icon: arrowDown,
+            iconColor: '#B04A48',
+            textColor: '#B04A48',
+            backgroundColor: '#FDE2E1'
+        }
+    ];
     return (
         <Card isBorderless isRounded>
             <VStack spacing={5}>
                 <Heading level={3}>Last 30 days</Heading>
                 <Grid columns={[1, 2, 3]} gap={0}>
-                    <Card style={{ padding: '10px' }}>
-                        <VStack spacing={3}>
-                            <Heading level={4} weight={500}>Total Orders</Heading>
-                            <HStack expanded={false}>
-                                <HStack justify="left" spacing={1}>
-                                    <Heading level={2}>71,987</Heading>
-                                    <Text>from 70,900</Text>
-                                </HStack>
-                                <Surface
-                                    style={{
-                                        backgroundColor: '#DCFCE6',
-                                        borderRadius: '10px',
-                                        padding: '0px 15px 0px 5px'
-                                    }}
-                                >
-                                    <HStack spacing={0}>
-                                        <Icon icon={arrowUp} fill="#22C55D"></Icon>
-                                        <Text color='#176434'>12%</Text>
+                    {cardData.map((data, index) => (
+                        <Card key={index} style={{ padding: '20px' }}>
+                            <VStack spacing={3}>
+                                <Heading level={5} weight={500}>{data.title}</Heading>
+                                <HStack>
+                                    <HStack justify="left" spacing={1}>
+                                        <Heading level={2} color="blue">{data.current}</Heading>
+                                        <Text>from {data.previous}</Text>
                                     </HStack>
-                                </Surface>
-                            </HStack>
-                        </VStack>
-                    </Card>
-
-                    <Card style={{ padding: '10px' }}>
-                        <VStack spacing={3}>
-                            <Heading level={4} weight={500}>Avg. Order Value</Heading>
-                            <HStack>
-                                <HStack justify="left" spacing={1}>
-                                    <Heading level={2}>₹50,000</Heading>
-                                    <Text>from ₹100,000</Text>
+                                    <Surface
+                                        style={{
+                                            backgroundColor: data.backgroundColor,
+                                            borderRadius: '10px',
+                                            padding: '0px 15px 0px 5px'
+                                        }}>
+                                        <HStack spacing={0}>
+                                            <Icon icon={data.icon} fill={data.iconColor} />
+                                            <Text color={data.textColor}>{data.percentage}</Text>
+                                        </HStack>
+                                    </Surface>
                                 </HStack>
-                                <Surface
-                                    style={{
-                                        backgroundColor: '#DCFCE6',
-                                        borderRadius: '10px',
-                                        padding: '0px 15px 0px 5px'
-                                    }}
-                                >
-                                    <HStack spacing={0}>
-                                        <Icon icon={arrowUp} fill="#22C55D"></Icon>
-                                        <Text color='#176434'>10%</Text>
-                                    </HStack>
-                                </Surface>
-                            </HStack>
-                        </VStack>
-                    </Card>
-
-                    <Card style={{ padding: '10px' }}>
-                        <VStack spacing={3}>
-                            <Heading level={4} weight={500}>Order Conversion Rate</Heading>
-                            <HStack>
-                                <HStack alignment="left" spacing={1}>
-                                    <Heading level={2}>24.57%</Heading>
-                                    <Text>from 28.62%</Text>
-                                </HStack>
-                                <Surface
-                                    style={{
-                                        backgroundColor: '#FDE2E1',
-                                        borderRadius: '10px',
-                                        padding: '0px 15px 0px 5px'
-                                    }}
-                                >
-                                    <HStack spacing={0}>
-                                        <Icon icon={arrowDown} fill="#B04A48"></Icon>
-                                        <Text color='#B04A48'>4.04%</Text>
-                                    </HStack>
-                                </Surface>
-                            </HStack>
-                        </VStack>
-                    </Card>
+                            </VStack>
+                        </Card>
+                    ))}
                 </Grid>
             </VStack>
         </Card>
@@ -106,8 +90,8 @@ function Stats2() {
 // @meta-start
 Stats2.meta = {
     title: 'Stats 2',
-    name:'Stats2',
-    category:'Stats',
+    name: 'Stats2',
+    category: 'Stats',
     path: '/Stats/stats2',
 };
 // @meta-end
