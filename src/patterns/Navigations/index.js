09af5e24096fecf4372c29d98245d3572adc6f34
deepakgithub11 <deepakboranaoffice@gmail.com>
2024-09-09 16:16:15 +0530

    remove: navigation

--- src/patterns/Navigations/index.js
+++ src/patterns/Navigations/index.js
@@ -3,4 +3,3 @@
  */
 export { default as Navigation1 } from './navigate1';
 export { default as Navigation2 } from './navigate2';
-export { default as Navigation3 } from './navigate3';
--- src/patterns/Navigations/navigate2.js
+++ src/patterns/Navigations/navigate2.js
@@ -7,104 +7,98 @@ import { useState } from "react";
  * WordPress dependencies.
  */
 import {
-    __experimentalNavigation as Navigation,
-    __experimentalNavigationGroup as NavigationGroup,
-    __experimentalNavigationItem as NavigationItem,
-    __experimentalNavigationMenu as NavigationMenu,
-    __experimentalHStack as HStack,
+    MenuGroup,
+    MenuItem,
     __experimentalVStack as VStack,
     __experimentalText as Text,
+    __experimentalHStack as HStack,
 } from "@wordpress/components";
+import { comment, home, pages, people, post, settings } from "@wordpress/icons";
 
 /**
- * Render Navigation
+ * Render Dashboard
  */
 function Navigation2() {
-
     const [activeItem, setItemActive] = useState('dashboard');
 
     const items = [
         {
             name: 'Dashboard',
             slug: 'dashboard',
+            icon: home
         },
         {
             name: 'Posts',
             slug: 'posts',
-            count: 5
+            count: 5,
+            icon: post
         },
         {
             name: 'Pages',
             slug: 'pages',
-            count: '20+'
+            count: '20+',
+            icon: pages
         },
         {
             name: 'Users',
             slug: 'users',
-            count: 7
+            count: 7,
+            icon: people
         },
         {
             name: 'Comments',
             slug: 'comments',
+            icon: comment
         },
         {
             name: 'Settings',
             slug: 'settings',
+            icon: settings
         }
     ];
 
     return (
-        <>
-            <Navigation activeItem={activeItem}>
-                <NavigationMenu>
-                    <HStack alignment="center">
-                        <NavigationGroup>
-                            <VStack spacing={4}>
-                                {items.map((item, index) => (
-                                    <NavigationItem
-                                        className="wpui_nav_item"
-                                        key={index}
-                                        item={item.slug}
-                                        title={item.name}
-                                        badge={
-                                            item.count && (
-                                                <Text
-                                                    style={{
-                                                        padding: '5px 10px',
-                                                        borderRadius: '20px',
-                                                        border: '1px solid grey',
-                                                        background: 'white',
-                                                    }}
-                                                >
-                                                    {item.count}
-                                                </Text>
-                                            )
-                                        }
-                                        onClick={() => setItemActive(item.slug)}
-                                        style={{ width: '20rem' }}
-                                    />
-                                ))}
-                            </VStack>
-                        </NavigationGroup>
-                    </HStack>
-                </NavigationMenu>
-            </Navigation>
-            <style>
-                {`
-                       .wpui_nav_item .components-navigation__item-badge {
-                             padding: 0px;
-                        }
-                    `}
-            </style>
-        </>
+        <MenuGroup>
+            <HStack alignment="center">
+                <VStack spacing={2}>
+                    {items.map((item) => (
+                        <MenuItem
+                            key={item.slug}
+                            icon={item.icon}
+                            iconPosition="left"
+                            info={
+                                item.count && (
+                                    <Text
+                                        style={{
+                                            padding: '2px 5px',
+                                            borderRadius: '20px',
+                                            border: '1px solid grey',
+                                            color: activeItem === item.slug ? 'white' : '',
+                                        }} >
+                                        {item.count}
+                                    </Text>
+                                )
+                            }
+                            onClick={() => setItemActive(item.slug)}
+                            isPressed={activeItem === item.slug}
+                            style={{
+                                width: '20rem',
+                                height: '100%',
+                            }}>
+                            {item.name}
+                        </MenuItem>
+                    ))}
+                </VStack>
+            </HStack>
+        </MenuGroup>
     );
 };
 
 // @meta-start
 Navigation2.meta = {
     title: 'Navigate 2',
-    name:'Navigation2',
-    category:'Navigations',
+    name: 'Navigation2',
+    category: 'Navigations',
     path: '/Navigations/navigate2',
 };
 // @meta-end
