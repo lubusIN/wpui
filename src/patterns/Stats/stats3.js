6d72e7878408ba00c135d8e2ff5d0ad55bfa8be5
deepakgithub11 <deepakboranaoffice@gmail.com>
2024-09-09 12:51:11 +0530

    refactor: stats 2

--- src/patterns/Stats/stats3.js
+++ src/patterns/Stats/stats3.js
@@ -8,44 +8,41 @@ import {
     __experimentalHeading as Heading,
     __experimentalGrid as Grid,
     __experimentalVStack as VStack,
-
 } from "@wordpress/components";
 
 /**
  * Render Stats
  */
 function Stats3() {
+    const statData = [
+        {
+            label: 'Total Orders',
+            value: '71,987'
+        },
+        {
+            label: 'Avg. Order Value',
+            value: '₹100,000'
+        },
+        {
+            label: 'Order Conversion Rate',
+            value: '24.57%'
+        }
+    ];
     return (
         <Card isBorderless isRounded>
             <VStack spacing={5}>
                 <Heading level={3}>Last 30 days</Heading>
                 <Grid columns={[1, 2, 3]} gap={0}>
-                    <Card>
-                        <CardBody>
-                            <VStack>
-                                <Text size={14}>Total Orders</Text>
-                                <Heading level={2}>71,987</Heading>
-                            </VStack>
-                        </CardBody>
-                    </Card>
-
-                    <Card>
-                        <CardBody>
-                            <VStack>
-                                <Text size={14}>Avg. Order Value</Text>
-                                <Heading level={2}>₹100,000</Heading>
-                            </VStack>
-                        </CardBody>
-                    </Card>
-
-                    <Card>
-                        <CardBody>
-                            <VStack>
-                                <Text size={14}>Order Conversion Rate</Text>
-                                <Heading level={2}>24.57%</Heading>
-                            </VStack>
-                        </CardBody>
-                    </Card>
+                    {statData.map((stat, index) => (
+                        <Card key={index}>
+                            <CardBody>
+                                <VStack>
+                                    <Text size={14}>{stat.label}</Text>
+                                    <Heading level={2}>{stat.value}</Heading>
+                                </VStack>
+                            </CardBody>
+                        </Card>
+                    ))}
                 </Grid>
             </VStack>
         </Card>
@@ -55,8 +52,8 @@ function Stats3() {
 // @meta-start
 Stats3.meta = {
     title: 'Stats 3',
-    name:'Stats3',
-    category:'Stats',
+    name: 'Stats3',
+    category: 'Stats',
     path: '/Stats/stats3',
 };
 // @meta-end
